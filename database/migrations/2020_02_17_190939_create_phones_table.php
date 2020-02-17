eea825f82bad579da8a9a8a6d2b47cff6a23cc6d
Paulo <paulo.macario@criarenet.com>
2020-02-17 16:37:25 -0300

    Start project

---
+++ database/migrations/2020_02_17_190939_create_phones_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('phone_one');
+            $table->string('phone_two');
+            $table->string('phone_tree');
+            $table->string('phone_four');
+            $table->string('phone_five');
+            $table->string('phone_six');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
